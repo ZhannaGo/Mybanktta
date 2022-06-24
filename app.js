573698b636d59089456b5229aac60eead3371ed5
DS83969 <zhannagolubeva@yahoo.com>
2022-06-24 01:23:58 -0500

    t

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001
 
 if (process.env.NODE_ENV==="production"){
     app.use(express.static('build'))
-    app.get("*", (req, res) => {
+    app.get("+", (req, res) => {
         res.sendFile(path.resolve(_dirname, 'build', 'index.html'))
     })
 }
